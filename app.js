479510095eab15cd55b5fa1bfe7f883857c74f32
Litpax-Technology <mis@litpaxtechnology.com>
2026-09-24 14:33:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 // API URL: change here if redeployed
 // ============================================================
 
-const API = 'https://script.google.com/macros/s/AKfycbyN0ZqQ3PUJE2jdq2NbCPPJqJy3lnaqCxSy-ZQJc6yRXQ-AzfX1FAEzv4Vmo2PfwSjO/exec';
+const API = 'https://script.google.com/macros/s/AKfycbxp6m0a2k87Rc9LPxxJRCOPFoUhFWqfUPxogqu2Ke2SEAHj0mvuUFU1751cppqdQSap/exec';
 
 function setEl(id, val) { const el = document.getElementById(id); if (el) el.textContent = val; }
 function showEl(id, show) { const el = document.getElementById(id); if (el) el.style.display = show ? 'inline' : 'none'; }
